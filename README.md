86bf76f1a955508652935ba947a5dcb36b8bc274
Deniz Eren Akkuzu <38756930+denizeakkuzu@users.noreply.github.com>
2022-08-30 20:07:03 +0300

    xx

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # qrmenu
-
-DENIZ iletişimi düzelttim ama senin barı bir güzel bozdum kanka yapmam gerekiyordu bir kaç bişey daha düzelttim bi ara bunu görürsen bana yaz
-
-menu
-
-Sevgili özgüç ....
-
-todo list oluşturulacak
-
--hamburger nav bar fixed
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+const namee = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+const message = document.getElementById("message").value;
+
 hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
@@ -12,3 +17,23 @@ document.querySelectorAll(".nav-link").forEach(n =>
 		navMenu.classList.remove("active");
 	})
 );
+
+document.getElementById("submitButton").onclick = function sendMail() {
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "cafepavese@gmail.com",
+		Password: "qjzo wphe bbak aplq",
+		To: "cafepavese@gmail.com",
+		From: "denizakkuzu45@gmail.com",
+		Subject: "This is the subject",
+		Body:
+			"Name : " +
+			namee +
+			" <br> Email : " +
+			email +
+			" <br> Telefon Numarası : " +
+			phone +
+			" <br> Mesaj : " +
+			message,
+	}).then(message => alert("mail gönderildi"));
+};
