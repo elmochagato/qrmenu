86af5b34b1d099eb535d89588ce1e780272828fc
elmochagato <ozgucapb45@gmail.com>
2022-09-03 08:25:17 +0300

    formatting

--- script.js
+++ script.js
@@ -4,31 +4,50 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-	hamburger.classList.toggle("active");
-	navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link").forEach(n =>
-	n.addEventListener("click", () => {
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-	})
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
 );
 
 // sicak menu
-"use strict";
+("use strict");
 let sicakIcecekler = [
-	{"isim":"Çay1","fiyat":1+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay2","fiyat":2+" ₺","icerik":"çay ve sıcak su","resim":"./images/galeriResim1.jpg"},
-	{"isim":"Çay3","fiyat":3+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay4","fiyat":4+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-]
+  {
+    isim: "Çay1",
+    fiyat: 1 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay2",
+    fiyat: 2 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/galeriResim1.jpg",
+  },
+  {
+    isim: "Çay3",
+    fiyat: 3 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay4",
+    fiyat: 4 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+];
 
 let sicakMenu = document.getElementById("sıcak-menu");
 
-
-for(let icecek of sicakIcecekler){
-let sicakIceceklerEkleme =  `
+for (let icecek of sicakIcecekler) {
+  let sicakIceceklerEkleme = `
 <div  class="icecekler">
 <div  class="cay">
 	<span class="cay-img">
@@ -42,23 +61,42 @@ let sicakIceceklerEkleme =  `
 </div>
 </div>`;
 
-sicakMenu.insertAdjacentHTML("beforeend",sicakIceceklerEkleme);
+  sicakMenu.insertAdjacentHTML("beforeend", sicakIceceklerEkleme);
 }
 
 // soguk menu
-"use strict";
+("use strict");
 let sogukIcecekler = [
-	{"isim":"Çay1","fiyat":1+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay2","fiyat":2+" ₺","icerik":"çay ve sıcak su","resim":"./images/galeriResim1.jpg"},
-	{"isim":"Çay3","fiyat":3+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay4","fiyat":4+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-]
+  {
+    isim: "Çay1",
+    fiyat: 1 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay2",
+    fiyat: 2 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/galeriResim1.jpg",
+  },
+  {
+    isim: "Çay3",
+    fiyat: 3 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay4",
+    fiyat: 4 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+];
 
 let sogukMenu = document.getElementById("soguk-menu");
 
-
-for(let icecek of sogukIcecekler){
-	let sogukIceceklerEkleme =  `
+for (let icecek of sogukIcecekler) {
+  let sogukIceceklerEkleme = `
 	<div  class="icecekler">
 	<div  class="cay">
 		<span class="cay-img">
@@ -71,24 +109,43 @@ for(let icecek of sogukIcecekler){
 				<div class="price">${icecek.fiyat}</div>
 	</div>
 	</div>`;
-	
-	sogukMenu.insertAdjacentHTML("beforeend",sogukIceceklerEkleme);
-	}
 
-// soguk menu
-"use strict";
+  sogukMenu.insertAdjacentHTML("beforeend", sogukIceceklerEkleme);
+}
+
+// atıştırma-menu
+("use strict");
 let atıstırmalıkMenu = [
-	{"isim":"Çay1","fiyat":1+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay2","fiyat":2+" ₺","icerik":"çay ve sıcak su","resim":"./images/galeriResim1.jpg"},
-	{"isim":"Çay3","fiyat":3+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-	{"isim":"Çay4","fiyat":4+" ₺","icerik":"çay ve sıcak su","resim":"./images/Turkish_tea2.jpg"},
-]
+  {
+    isim: "Çay1",
+    fiyat: 1 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay2",
+    fiyat: 2 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/galeriResim1.jpg",
+  },
+  {
+    isim: "Çay3",
+    fiyat: 3 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+  {
+    isim: "Çay4",
+    fiyat: 4 + " ₺",
+    icerik: "çay ve sıcak su",
+    resim: "./images/Turkish_tea2.jpg",
+  },
+];
 
 let atıstırmaMenu = document.getElementById("atıstırma-menu");
 
-
-for(let icecek of atıstırmalıkMenu){
-	let atıstırmalıkMenuEkleme =  `
+for (let icecek of atıstırmalıkMenu) {
+  let atıstırmalıkMenuEkleme = `
 	<div  class="icecekler">
 	<div  class="cay">
 		<span class="cay-img">
@@ -101,6 +158,6 @@ for(let icecek of atıstırmalıkMenu){
 				<div class="price">${icecek.fiyat}</div>
 	</div>
 	</div>`;
-	
-	atıstırmaMenu.insertAdjacentHTML("beforeend",atıstırmalıkMenuEkleme);
-	}
+
+  atıstırmaMenu.insertAdjacentHTML("beforeend", atıstırmalıkMenuEkleme);
+}
