d661eed2c0bf94c02744f772a68503dc9989608b
elmochagato <ozgucapb45@gmail.com>
2022-09-06 08:08:38 +0300

    zzzz

--- script.js
+++ script.js
@@ -98,3 +98,26 @@ for (let atistirma of atistirmaliklar) {
   /* Bu bar beni sikecek */
 /* Bu bar beni sikecek */
 /* Bu bar beni sikecek */
+// Get the container element
+var btnContainer = document.getElementById("scrt");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("scrolling-kartlar");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+
+
